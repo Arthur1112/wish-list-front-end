fb603eb6a507e1494c611d0cafee1cac7695728d
Arthur <arthur_2335@hotmail.com>
2022-05-31 11:40:57 -0400

    created and exported getWishes in wishes.ts

--- src/services/client.ts
+++ src/services/client.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const getClient = () => {
   return axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3000", //talking to backend
   });
 };
