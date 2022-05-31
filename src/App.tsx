ebba3b0056436d632f8c2e188adbd1be87b287fb
Arthur <arthur_2335@hotmail.com>
2022-05-31 11:31:31 -0400

    added theme to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { CssBaseline, createTheme, ThemeProvider } from "@mui/material";
 import { FC } from "react";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: `'Noto Sans', sans-serif`,
+    fontWeightRegular: 600,
+  },
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#131924",
+    },
+    primary: {
+      main: "#4d88ff",
+    },
+    secondary: {
+      main: "#a9a9a9",
+    },
+  },
+});
+
 export const App: FC = () => {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
+  );
 };
